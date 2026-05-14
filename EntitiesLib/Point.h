b6f11e418b1c52f8d2d47f4c02fb69c39f24eca1
shirke_PC <1365036720@qq.com>
2026-05-14 18:23:20 +0800

    1

--- EntitiesLib/Point.h
+++ EntitiesLib/Point.h
@@ -4,7 +4,14 @@
 // ─── Point ──────────────────────────────────────────────────
 struct EntityPoint
 {
+    // 图元属性
     EntityProp prop;
     Vertex3D   point;
     double     angle = 0.0;
+
+    //计算边界
+    QRectF boundingBox(double padding = 0.0) const {
+        return QRectF(point.x() - padding, point.y() - padding, padding * 2, padding * 2);
+    }
 };
+
